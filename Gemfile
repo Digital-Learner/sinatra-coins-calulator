c53fb1a9125dc6453c8559e7cf658c4e78b9969d
Digital-Learner <digital.learning.journerys@gmail.com>
2014-05-30 12:59:42 +0100

    Basic setup - adding gems, guard, rspec

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'rack'
+gem 'thin'
+
+group :development do
+  gem 'shotgun'
+  gem 'rake'
+  gem 'pry'
+  gem 'pry-nav'
+  gem 'pry-stack_explorer'
+  gem 'guard-rspec'
+end
+
+group :test do
+  gem 'rspec'
+  gem 'rack-test'
+  gem 'capybara', require: false
+  gem 'simplecov', require: false
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'pry'
+require 'simplecov'
+
+SimpleCov.start
+
+ENV['RACK_ENV'] = 'test'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = 'random'
+
+  config.around(:each, :fullstack => true) do |example|
+    example.run
+  end
+end
