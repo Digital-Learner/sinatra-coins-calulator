389c239c4918b5aebd3fe83b81ed7913115f83b2
Digital-Learner <digital.learning.journerys@gmail.com>
2014-05-30 20:06:51 +0100

    Add an image for the application
    - set public & static configuration options so the image is rendered by
      modular Sinatra App

--- app/coin_calculator.rb
+++ app/coin_calculator.rb
@@ -2,6 +2,10 @@ require 'sinatra/base'
 
 module CoinCalculator
   class Main < Sinatra::Base
+
+    set :static, true
+    set :public_dir, './public'
+
     get '/' do
       erb :index
     end
--- spec/features/coin_calculator_spec.rb
+++ spec/features/coin_calculator_spec.rb
@@ -1,10 +1,10 @@
 require 'feature_helper'
 
 describe CoinCalculator::Main, type: :feature do
-  # let(:application) { CoinCalculator::Main.new }
 
   it 'shows the main page' do
     visit '/'
-    expect(page). to have_content 'A simple Coin Calculator'
+    expect(page).to have_content 'A simple Coin Calculator'
+    expect(page).to have_title 'UK Coin Calculator'
   end
 end
