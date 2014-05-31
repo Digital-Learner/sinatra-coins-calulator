bcd76c891a9dc7be690c8b8762649d1f605c5563
Digital-Learner <digital.learning.journerys@gmail.com>
2014-05-31 09:50:01 +0100

    Extend #has_image? matcher
    - include matching of ‘alt’ when present
    - both src & ing must be true to return true

--- spec/features/coin_calculator_spec.rb
+++ spec/features/coin_calculator_spec.rb
@@ -6,5 +6,6 @@ describe CoinCalculator::Main, type: :feature do
     visit '/'
     expect(page).to have_content 'A simple Coin Calculator'
     expect(page).to have_title 'UK Coin Calculator'
+    expect(page).to have_image('2013-Underground.jpeg', '2013 Underground 2 pound coin')
   end
 end
