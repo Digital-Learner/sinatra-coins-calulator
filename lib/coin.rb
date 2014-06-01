d9a5db41e6383e15729163f9ee6f16c5194a5b66
Digital-Learner <digital.learning.journerys@gmail.com>
2014-06-02 00:32:44 +0100

    Add test for double digit (85)
    - Add constant DENOMINATIONS_IN_PENCE as hash of coin name & amount in pence
    - Refactor #calculate_minimum_coins to use constant
    - Add #coin to add the number of coins for a denomination to the @uk_coins instance variable

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,6 +1,13 @@
 module CoinCalculator
   class Coin
     attr_reader :input
+
+    DENOMINATIONS_IN_PENCE = { two_pound: 200, one_pound: 100, fifty_pence: 50, twenty_pence: 20, two_pence: 2, one_pence: 1 }
+
+    def initialize
+      @uk_coins = {}
+    end
+
     def return_minimum_coins(requested_amount)
       validate_input(requested_amount)
     end
@@ -14,13 +21,19 @@ module CoinCalculator
     end
 
     def calculate_minimum_coins(pence)
-      pence = pence.to_i
+      total_pence = pence.to_i
+      remaining_pence = total_pence
 
-      two_pence = pence / 2
-      one_pence = pence % 2
+      DENOMINATIONS_IN_PENCE.keys.each do |k|
+        remaining_pence = coin(k, remaining_pence)
+      end
+      return @uk_coins
+    end
 
-      if (two_pence * 2) + (one_pence * 1) == pence
-        coins = { '2_pence' => two_pence, '1_pence' => one_pence }
+    def coin(denomination, amount)
+      if DENOMINATIONS_IN_PENCE.keys.include?(denomination.to_sym)
+        @uk_coins["#{denomination}"] = amount / DENOMINATIONS_IN_PENCE[denomination.to_sym]
+        return amount % DENOMINATIONS_IN_PENCE[denomination.to_sym]
       end
     end
   end
--- spec/lib/coins_spec.rb
+++ spec/lib/coins_spec.rb
@@ -6,16 +6,27 @@ describe CoinCalculator::Coin do
   end
 
   context 'single digit' do
-    it 'returns the correct number of 1 & 2 pence coins' do
+    it "returns the correct number of 1 & 2 pence coins for '4'" do
       pennies = "4"
-      coins = { '2_pence' => 2, '1_pence' => 0 }
+      coins = { "two_pound" => 0, "one_pound" => 0, "fifty_pence" => 0, "twenty_pence" => 0, "two_pence" => 2, "one_pence" => 0 }
 
       expect(@coins.return_minimum_coins(pennies)).to eql(coins)
+    end
 
+    it "returns the correct number of 1 & 2 pence coins for '9'" do
       pennies = "9"
-      coins = { '2_pence' => 4, '1_pence' => 1 }
+      coins = { "two_pound" => 0, "one_pound" => 0, "fifty_pence" => 0, "twenty_pence" => 0, "two_pence" => 4, "one_pence" => 1 }
 
       expect(@coins.return_minimum_coins(pennies)).to eql(coins)
+    end
+  end
+
+  context 'double digit' do
+    it "returns the correct number of 1, 2, 20 & 50 pence coins for '85'" do
+      pennies = "85"
+      expected_coins = { "two_pound" => 0, "one_pound" => 0, "fifty_pence" => 1, "twenty_pence" => 1, "two_pence" => 7, "one_pence" => 1 }
+
+      expect(@coins.return_minimum_coins(pennies)).to eq(expected_coins)
     end
   end
 end
