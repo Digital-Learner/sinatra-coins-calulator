f15a93e89aef686b46b088532e6dd7f0b3374d34
Digital-Learner <digital.learning.journerys@gmail.com>
2014-06-02 00:40:36 +0100

    Add test for pence symbol (197p)

--- spec/lib/coins_spec.rb
+++ spec/lib/coins_spec.rb
@@ -8,16 +8,16 @@ describe CoinCalculator::Coin do
   context 'single digit' do
     it "returns the correct number of 1 & 2 pence coins for '4'" do
       pennies = "4"
-      coins = { "two_pound" => 0, "one_pound" => 0, "fifty_pence" => 0, "twenty_pence" => 0, "two_pence" => 2, "one_pence" => 0 }
+      expected_coins = { "two_pound" => 0, "one_pound" => 0, "fifty_pence" => 0, "twenty_pence" => 0, "two_pence" => 2, "one_pence" => 0 }
 
-      expect(@coins.return_minimum_coins(pennies)).to eql(coins)
+      expect(@coins.return_minimum_coins(pennies)).to eql(expected_coins)
     end
 
     it "returns the correct number of 1 & 2 pence coins for '9'" do
       pennies = "9"
-      coins = { "two_pound" => 0, "one_pound" => 0, "fifty_pence" => 0, "twenty_pence" => 0, "two_pence" => 4, "one_pence" => 1 }
+      expected_coins = { "two_pound" => 0, "one_pound" => 0, "fifty_pence" => 0, "twenty_pence" => 0, "two_pence" => 4, "one_pence" => 1 }
 
-      expect(@coins.return_minimum_coins(pennies)).to eql(coins)
+      expect(@coins.return_minimum_coins(pennies)).to eql(expected_coins)
     end
   end
 
@@ -26,7 +26,16 @@ describe CoinCalculator::Coin do
       pennies = "85"
       expected_coins = { "two_pound" => 0, "one_pound" => 0, "fifty_pence" => 1, "twenty_pence" => 1, "two_pence" => 7, "one_pence" => 1 }
 
-      expect(@coins.return_minimum_coins(pennies)).to eq(expected_coins)
+      expect(@coins.return_minimum_coins(pennies)).to eql(expected_coins)
+    end
+  end
+
+  context 'pence symbol' do
+    it "returns the correct number of 1, 2, 20 & 50 pence coins for '197p'" do
+      pennies = "197p"
+      expected_coins = { "two_pound" => 0, "one_pound" => 1, "fifty_pence" => 1, "twenty_pence" => 2, "two_pence" => 3, "one_pence" => 1 }
+
+      expect(@coins.return_minimum_coins(pennies)).to eql(expected_coins)
     end
   end
 end
