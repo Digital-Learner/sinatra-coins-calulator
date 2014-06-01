8860463bed2405b4238f2812e400ad21525542c0
Digital-Learner <digital.learning.journerys@gmail.com>
2014-06-01 19:02:07 +0100

    Make ‘single digit’ test pass
    - Rename #calculate_mimimum_coins to #return_mimimum_coins
      as semantically more appropriate & original method name can be reused
      in more appropriate method

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,7 +1,27 @@
 module CoinCalculator
   class Coin
-    def calculate_minimum_coins(requested_amount)
-      coins = { '2_pence' => 2 }
+    attr_reader :input
+    def return_minimum_coins(requested_amount)
+      validate_input(requested_amount)
+    end
+
+    def validate_input(data)
+      if data =~ /\d/
+        calculate_minimum_coins(data)
+      else
+        return {}
+      end
+    end
+
+    def calculate_minimum_coins(pence)
+      pence = pence.to_i
+
+      two_pence = pence / 2
+      one_pence = pence % 2
+
+      if (two_pence * 2) + (one_pence * 1) == pence
+        coins = { '2_pence' => two_pence, '1_pence' => one_pence }
+      end
     end
   end
 end
