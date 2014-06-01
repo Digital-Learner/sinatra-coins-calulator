6403e310feb2963bb0ed7136bd5295f5416a1b7d
Digital-Learner <digital.learning.journerys@gmail.com>
2014-06-02 00:48:39 +0100

    Add test for pence symbol single digit (2p)

--- spec/lib/coins_spec.rb
+++ spec/lib/coins_spec.rb
@@ -22,7 +22,7 @@ describe CoinCalculator::Coin do
   end
 
   context 'double digit' do
-    it "returns the correct number of 1, 2, 20 & 50 pence coins for '85'" do
+    it "returns the correct number of uk coins for '85'" do
       pennies = "85"
       expected_coins = { "two_pound" => 0, "one_pound" => 0, "fifty_pence" => 1, "twenty_pence" => 1, "two_pence" => 7, "one_pence" => 1 }
 
@@ -31,11 +31,20 @@ describe CoinCalculator::Coin do
   end
 
   context 'pence symbol' do
-    it "returns the correct number of 1, 2, 20 & 50 pence coins for '197p'" do
+    it "returns the correct number of uk coins for '197p'" do
       pennies = "197p"
       expected_coins = { "two_pound" => 0, "one_pound" => 1, "fifty_pence" => 1, "twenty_pence" => 2, "two_pence" => 3, "one_pence" => 1 }
 
       expect(@coins.return_minimum_coins(pennies)).to eql(expected_coins)
+    end
+
+    context 'single digit' do
+      it "returns the correct number of uk coins for '2p'" do
+        pennies = "2p"
+        expected_coins = { "two_pound" => 0, "one_pound" => 0, "fifty_pence" => 0, "twenty_pence" => 0, "two_pence" => 1, "one_pence" => 0 }
+
+        expect(@coins.return_minimum_coins(pennies)).to eql(expected_coins)
+      end
     end
   end
 end
