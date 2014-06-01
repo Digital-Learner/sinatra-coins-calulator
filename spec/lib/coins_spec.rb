b9da46906e332792b43607a8472fc9c1e2c11fe6
Digital-Learner <digital.learning.journerys@gmail.com>
2014-06-01 19:05:16 +0100

    Add additional test for single digit (9 pence)

--- spec/lib/coins_spec.rb
+++ spec/lib/coins_spec.rb
@@ -11,6 +11,11 @@ describe CoinCalculator::Coin do
       coins = { '2_pence' => 2, '1_pence' => 0 }
 
       expect(@coins.return_minimum_coins(pennies)).to eql(coins)
+
+      pennies = "9"
+      coins = { '2_pence' => 4, '1_pence' => 1 }
+
+      expect(@coins.return_minimum_coins(pennies)).to eql(coins)
     end
   end
 end
