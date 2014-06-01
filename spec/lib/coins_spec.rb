93361b8974758bd4a5a24daeb0cc00daf1f2732e
Digital-Learner <digital.learning.journerys@gmail.com>
2014-06-01 19:02:07 +0100

    Make ‘single digit’ test pass
    - Rename #calculate_mimimum_coins to #return_mimimum_coins
      as semantically more appropriate & original method name can be reused
      in more appropriate method

--- spec/lib/coins_spec.rb
+++ spec/lib/coins_spec.rb
@@ -7,10 +7,10 @@ describe CoinCalculator::Coin do
 
   context 'single digit' do
     it 'returns the correct number of 1 & 2 pence coins' do
-      pennies = 4
-      coins = { '2_pence' => 2 }
+      pennies = "4"
+      coins = { '2_pence' => 2, '1_pence' => 0 }
 
-      expect(@coins.calculate_minimum_coins(pennies)).to eql(coins)
+      expect(@coins.return_minimum_coins(pennies)).to eql(coins)
     end
   end
 end
