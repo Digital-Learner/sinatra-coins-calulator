84e796461bd19986ec70a48815a0ef66cc4390cb
Digital-Learner <digital.learning.journerys@gmail.com>
2014-06-02 09:29:52 +0100

    Wire-up front end to display results for user entry
    - connect to the coin class
    - display the data in rough table (no css)

--- app/coin_calculator.rb
+++ app/coin_calculator.rb
@@ -1,10 +1,12 @@
 require 'sinatra/base'
+require 'coin'
 
 module CoinCalculator
   class Main < Sinatra::Base
 
     set :static, true
     set :public_dir, './public'
+    enable :logging
 
     get '/' do
       erb :index
@@ -12,6 +14,7 @@ module CoinCalculator
 
     post '/calculate' do
       @pennies = params[:pennies]
+      @result = CoinCalculator::Coin.new.return_minimum_coins(@pennies)
       erb :result
     end
   end
