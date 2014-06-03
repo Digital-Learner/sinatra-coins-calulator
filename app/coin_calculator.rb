a8a63a5c62d29866aee192656c61a9985d4c1af4
Digital-Learner <digital.learning.journerys@gmail.com>
2014-06-03 11:01:13 +0100

    Register sinatra-assetpack
    - change location of js as using ‘assets’ directory
    - create subdirectories for js, scss

--- app/coin_calculator.rb
+++ app/coin_calculator.rb
@@ -1,8 +1,30 @@
 require 'sinatra/base'
+require 'sinatra/assetpack'
 require 'coin'
 
 module CoinCalculator
   class Main < Sinatra::Base
+
+    register Sinatra::AssetPack
+    assets do
+      serve '/js', from: 'assets/js'
+      serve '/bower_components', from: 'bower_components'
+
+      js :modernizr, [
+        '/bower_components/modernizr/modernizr.js',
+      ]
+
+      js :libs, [
+        '/bower_components/jquery/dist/jquery.js',
+        '/bower_components/foundation/js/foundation.js'
+      ]
+
+      js :application, [
+        '/js/app.js'
+      ]
+
+      js_compression :jsmin
+    end
 
     set :static, true
     set :public_dir, './public'
