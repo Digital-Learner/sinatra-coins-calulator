86ee3275751112af767fee31913bf24de66f16d4
Digital-Learner <digital.learning.journeys@gmail.com>
2014-06-04 02:19:18 +0100

    Wire-up flash/alert js
    - hide alert-box when no errors (expected state at start)
    - when flash:
        success, display using foundation success default
        info, display using foundation info default
        warning, display using foundation warning default
        error, display using foundation error default

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,3 +1,47 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$('div.alert-box').hide();
+
+var sinatraFlashType = {
+  "success": function() {
+    $('.alert-box').show();
+    $('.alert-box').addClass('success');
+  },
+
+  "info": function() {
+    $('.alert-box').show();
+    $('.alert-box').addClass('info');
+  },
+
+  "error": function() {
+    $('.alert-box').show();
+    $('.alert-box').addClass('alert');
+  },
+
+  "warning": function() {
+    $('.alert-box').show();
+    $('.alert-box').addClass('warning');
+  },
+
+  "default": function() {
+  }
+};
+
+// Check what the .flash class property is.
+if ( $('div.flash').hasClass('success') ) {
+    sinatraFlashType[ "success" ]();
+
+} else if ( $('.flash').hasClass('info') ) {
+    sinatraFlashType[ "info" ]();
+
+} else if ( $('.flash').hasClass('error') ) {
+    sinatraFlashType[ "error" ]();
+
+} else if ( $('.flash').hasClass('warning') ) {
+    sinatraFlashType[ "warning" ]();
+
+} else {
+    sinatraFlashType[ "default" ]();
+};
