042978d17caf65e6bb411b693d9d4a9c0ea5646b
Digital-Learner <digital.learning.journeys@gmail.com>
2014-06-04 11:56:25 +0100

    Style Sinatra-Flash messages

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -4,25 +4,30 @@ $(document).foundation();
 
 $('div.alert-box').hide();
 
+// <-- Sinatra-Flash behaviour : Start -->
 var sinatraFlashType = {
   "success": function() {
     $('.alert-box').show();
     $('.alert-box').addClass('success');
+    $('#main-h1-heading').hide();
   },
 
   "info": function() {
     $('.alert-box').show();
     $('.alert-box').addClass('info');
+    $('#main-h1-heading').hide();
   },
 
   "error": function() {
     $('.alert-box').show();
     $('.alert-box').addClass('alert');
+    $('#main-h1-heading').hide();
   },
 
   "warning": function() {
     $('.alert-box').show();
     $('.alert-box').addClass('warning');
+    $('#main-h1-heading').hide();
   },
 
   "default": function() {
